3f1518335d0184d7feca2d58c2eced05f2e92784
copko <copko@spseke.sk>
2025-04-02 22:53:46 +0200

    Update config.php - údaje pre pripojenie k DB

--- config.php
+++ config.php
@@ -2,15 +2,15 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
+define('DB_USERNAME', 'eshop');
+define('DB_PASSWORD', 'Teples3kovaneponozky');
+define('DB_NAME', 'eshop');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 // Check connection
 if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+    die("ERROR: Nemôžem sa pripojiť. " . mysqli_connect_error());
 }
 ?>
